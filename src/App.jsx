5f9f58ba3730b5381c9eea3b46f2c8f0fcee4bff
Monalisa Sabina <onyangomonalisa@hotmail.com>
2026-03-25 19:06:38 +0300

    scores

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,85 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
+import dingSound from "./assets/Sounds/freesound_community-ding-101492.mp3"
 
 function App(){
 
   const [userChoice, setUserChoice] = useState("");
   const [computerChoice, setComputerChoice] = useState("");
-
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [result, setResult] = useState("");
+  
+ 
+  // Computer Choices
   const compChoices = ["Rock", "Paper","Scissors"]
+
+  // Sounds
+  const dingAudio = useRef(new Audio(dingSound)) ;
 
   // Generate a random choice for the computer
   function computerBrain(){
+
+    // Choose randomly
     const randomIndex = Math.floor(Math.random() * compChoices.length);
-    setComputerChoice(compChoices[randomIndex])
+
+    const choice = compChoices[randomIndex];
+
+    setComputerChoice(choice);
+
+    return choice;
   }
 
-  // Underrstand the game logic and determine the winner
+  // Determining the winner
   // Rock beats Scissors, Scissors beats Paper, and Paper beats Rock. If both players choose the same option, it's a tie.
-  function determineWinner(){
+  function determineWinner( user, computer){
 
-    if (userChoice === "" || computerChoice === "") {
+    if (user === "" || computer === "") {
       return "Please make a choice!";
     }
 
-    if (userChoice === computerChoice) {
+    if (user === computer) {
        return "It's a tie!";
     }
 
-
-    if ( userChoice === "Rock" && computerChoice === "Scissors" ||
-       userChoice === "Scissors" && computerChoice === "Paper" ||
-       userChoice === "Paper" && computerChoice === "Rock" ) {
-         return "You win!";
+    if ( user === "Rock" && computer === "Scissors" ||
+         user === "Scissors" && computer === "Paper" ||
+         user === "Paper" && computer === "Rock"
+      ) {
+          setUserScore(prev => prev + 1);  
+          return "You win!" ;
        } else {
+          setComputerScore(prev => prev + 1);
          return "Computer wins!";
        }
   }
 
+
+  // Handle User Click
+  function handleChoice(choice){
+
+    // user choice
+    setUserChoice(choice);
+
+    // Computer choice
+    const compChoice = computerBrain();
+
+    // Play sound
+    dingAudio.current.play();
+
+    // Outcome
+    const outcome = determineWinner(choice, compChoice);
+    setResult(outcome);
+  }
+
+   
   // Reset the game state to allow for a new round
   function resetGame(){
     setUserChoice("");
     setComputerChoice("");
+    setResult("");
+    setComputerScore(0);
+    setUserScore(0);
   } 
   
 
@@ -51,11 +91,16 @@ function App(){
 
         <h1>ROCK PAPER SCISSORS</h1>
 
+        {/* Scoreboard */}
+        <div className="scoreboard">
+            <p> You: {userScore} | Computer: {computerScore} </p>
+        </div>
+
         {/* USER CHOICE */}
         <div className="app_btn">
-          <button onClick={() => { setUserChoice("Rock"); computerBrain(); }}>Rock 🪨</button>
-          <button onClick={() => { setUserChoice("Paper"); computerBrain(); }}>Paper 📜</button>
-          <button onClick={() => { setUserChoice("Scissors"); computerBrain(); }}>Scissors ✂️</button>
+          <button onClick={() => { handleChoice("Rock")}}>Rock 🪨</button>
+          <button onClick={() => { handleChoice("Paper")}}>Paper 📜</button>
+          <button onClick={() => { handleChoice("Scissors")}}>Scissors ✂️</button>
         </div>
 
         <p>You chose: <span>{userChoice}</span></p>
@@ -64,7 +109,7 @@ function App(){
         <p>Computer chose: <span>{computerChoice}</span></p>
 
         {/* Result */}
-        <p className="result"> {determineWinner()} </p>
+        <p className="result"> {result} </p>
 
         <button id="reset_btn" onClick={resetGame}>Play Again</button>  
 
